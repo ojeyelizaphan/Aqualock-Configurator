9bebb12e5db2176cbe84030ad4a2a92cf55c47f9
Ojey Jabuya <ojabuya@standardmedia.co.ke>
2025-07-18 18:02:40 +0300

    eager load ConfigurationWizard

--- resources/js/app.js
+++ resources/js/app.js
@@ -11,11 +11,14 @@ const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) =>
-        resolvePageComponent(
-            `./Pages/${name}.vue`,
-            import.meta.glob('./Pages/**/*.vue'),
-        ),
+    resolve: (name) =>{
+        if (name === 'Configurations/ConfigurationWizard') {
+            return import('./Pages/Configurations/ConfigurationWizard.vue'); // eager static import
+        }
+
+        const pages = import.meta.glob('./Pages/**/*.vue'); // lazy load others
+        return resolvePageComponent(`./Pages/${name}.vue`, pages);
+    },
     setup({ el, App, props, plugin }) {
         return createApp({ render: () => h(App, props) })
             .use(plugin)
